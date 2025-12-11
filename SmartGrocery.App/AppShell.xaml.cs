5639137c1e882935d5ae14c0b87961374a7b21d2
Renata Navarro <renatanavarro@me.com>
2025-12-11 01:51:10 +0000

    feat: initial project structure
    -created solution, folders and first cs files:
    SmartGrocery.App
    Views
    SmartGrocery.Core
    Models
    Interfaces
    SmartGrocery.Infrastructure
    Data
    
    Repositories
    Services
    SmartGrocery.Tests
    
    -added first cs files:
    Item.cs
    ShoppingList.cs
    IRecommendationService.cs
    LocalDatabase.cs
    SqliteRepository.cs
    LocalRecommendationService.cs
    RecommendationTests.cs
    
    - added README.md
    - setup .gitignore

---
+++ SmartGrocery.App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartGrocery.App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SmartGrocery.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SmartGrocery.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
